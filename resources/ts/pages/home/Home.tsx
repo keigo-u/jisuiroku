8b5f84c8b39f84c780f65313deffdd51934ad5dc
keigo unuma <keigo.u88@gmail.com>
2023-04-17 21:41:35 +0900

    ボタンの位置調整

--- resources/ts/pages/home/Home.tsx
+++ resources/ts/pages/home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useBooks, useCreateBook } from "../../queries/BookQuery";
 import { Book } from "../../types/Book";
 import Card from "./Card";
@@ -18,17 +18,17 @@ const HomePage: React.FC = () => {
 
     return(
         <>
-        <div className="text-2xl m-5">Home</div>
+        <div className="text-2xl m-5">じすいろく一覧</div>
+
+        <button type="button" className="py-3 px-4 ml-3 mb-3 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800" data-hs-overlay="#hs-medium-modal">
+            新規作成
+        </button>
 
         <div className="flex flex-wrap">
             { books.map((book: Book) => (
                 <Card book={book} key={book.id} />
             ))}
         </div>
-
-        <button type="button" className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800" data-hs-overlay="#hs-medium-modal">
-            新規作成
-        </button>
 
         <FormModal />
         </>
