edfd6afc0ce9cda8204320ff7d18ca283daf0d55
keigo unuma <keigo.u88@gmail.com>
2023-06-24 19:21:22 +0900

    routeにnameを追加

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('register', [LoginController::class, 'register']);
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/logout', [LoginController::class, 'logout']);
+Route::post('register', [LoginController::class, 'register'])->name('register');
+Route::post('/login', [LoginController::class, 'login'])->name('login');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::middleware('auth:sanctum')->group(function() {
 
