87acc66cd0136e85023d198012da78f57e85c77a
keigo unuma <keigo.u88@gmail.com>
2023-07-30 18:47:43 +0900

    登録時にもOAuthが利用できるように変更

--- resources/ts/pages/auth/Register.tsx
+++ resources/ts/pages/auth/Register.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { useRegister } from "../../queries/AuthQuery";
+import { useGetOAuthUrl, useRegister } from "../../queries/AuthQuery";
 import { Helmet } from "react-helmet-async";
 
 const RegisterPage = () => {
@@ -11,11 +11,16 @@ const RegisterPage = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [password_confirmation, setPasswordConfirmation] = useState('')
+    const getOAuthUrl = useGetOAuthUrl()
 
     const handleRegister = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         console.log({ name, email, password, password_confirmation })
         register.mutate({ name:name, email:email, password:password, password_confirmation:password_confirmation })
+    }
+
+    const handleLoginOnGoole = () => {
+        getOAuthUrl.mutate({ provider: 'google' })
     }
 
     return (
@@ -36,7 +41,7 @@ const RegisterPage = () => {
                 </div>
 
                 <div className="mt-5">
-                    <button type="button" className="w-full py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-gray-800 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800">
+                    <button onClick={handleLoginOnGoole} type="button" className="w-full py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-gray-800 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800">
                     <svg className="w-4 h-auto" width="46" height="47" viewBox="0 0 46 47" fill="none">
                         <path d="M46 24.0287C46 22.09 45.8533 20.68 45.5013 19.2112H23.4694V27.9356H36.4069C36.1429 30.1094 34.7347 33.37 31.5957 35.5731L31.5663 35.8669L38.5191 41.2719L38.9885 41.3306C43.4477 37.2181 46 31.1669 46 24.0287Z" fill="#4285F4"/>
                         <path d="M23.4694 47C29.8061 47 35.1161 44.9144 39.0179 41.3012L31.625 35.5437C29.6301 36.9244 26.9898 37.8937 23.4987 37.8937C17.2793 37.8937 12.0281 33.7812 10.1505 28.1412L9.88649 28.1706L2.61097 33.7812L2.52296 34.0456C6.36608 41.7125 14.287 47 23.4694 47Z" fill="#34A853"/>
